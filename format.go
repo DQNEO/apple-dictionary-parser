a5edaf7abfeba24e90974661abb1f92f59be07a8
DQNEO <dqneoo@gmail.com>
2023-01-29 22:18:34 +0900

    Parse hg class

--- format.go
+++ format.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"gopkg.in/yaml.v3"
 	"os"
 	"strings"
 
@@ -15,6 +16,19 @@ const SPLT = "\t"
 type RawEntry struct {
 	Title string
 	Body  []byte
+}
+
+type EntryAsText struct {
+	Title string
+	//DEBUG_HG    string
+	X_Syllable  string
+	X_IPA       string
+	SG          string
+	Phrases     string
+	PhVerbs     string
+	Derivatives string
+	Etym        string
+	Note        string
 }
 
 type ParsedEntry struct {
@@ -155,9 +169,9 @@ func renderText(entries []*RawEntry) {
 			}
 			class := elm.SelectAttr("class").Value
 			switch class {
-			case "hg x_xh0":
+			case "hg x_xh0": // head group
 				pe.HG = elm
-			case "sg":
+			case "sg": // meaning
 				pe.SG = elm
 			case "subEntryBlock x_xo0 t_phrases":
 				pe.Phrases = elm
@@ -179,8 +193,122 @@ func renderText(entries []*RawEntry) {
 }
 
 func asText(pe *ParsedEntry) {
-	fmt.Printf("---\n%s\n  %s\n  %s\n  %s\n  %s\n  %s\n  %s\n  %s\n",
-		pe.Title, S(pe.HG), S(pe.SG), S(pe.Phrases), S(pe.PhVerbs), S(pe.Derivatives), S(pe.Etym), S(pe.Note))
+	hg := parseHG(pe.Title, pe.HG)
+	//hgDump, err := yaml.Marshal(hg)
+	et := &EntryAsText{
+		Title: pe.Title,
+		//HG:          string(hgDump),
+		X_Syllable:  hg.SYL_TXT,
+		X_IPA:       hg.PRX,
+		SG:          S(pe.SG),
+		Phrases:     S(pe.Phrases),
+		PhVerbs:     S(pe.PhVerbs),
+		Derivatives: S(pe.Derivatives),
+		Etym:        S(pe.Etym),
+		Note:        S(pe.Note),
+	}
+	d, err := yaml.Marshal(et)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(d))
+}
+
+type HG struct {
+	HW      string
+	SYL_TXT string
+	PRX     string
+	PR      string
+	VG      string
+	LG      string
+	FG      string
+}
+
+func parseHG(title string, eHG *etree.Element) *HG {
+	if eHG == nil {
+		panic("HG is nil")
+	}
+	tokens := eHG.Child
+	hg := &HG{}
+	for _, tok := range tokens {
+		elm, ok := tok.(*etree.Element)
+		if ok {
+			cls := elm.SelectAttr("class")
+			className := cls.Value
+			switch className {
+			case "hw": // This is always present
+				hg.HW = dumpElm(elm)
+			case "syl_txt":
+				hg.SYL_TXT = elm.Text()
+			case "prx":
+				// len(elm.Child) varies among 2,4,6,8,12
+				// ['|', option1-US, option1-IPA, option2-US, option2-IPA, ..., '|]
+				var pronunciation []string
+				if len(elm.Child) > 2 {
+					options := elm.Child[1 : len(elm.Child)-1] // remove enclosing "| |" pair.
+					for i := 1; i < len(options); i += 2 {
+						elm, ok := options[i].(*etree.Element)
+						if !ok {
+							panic("Unexpected prx token")
+						}
+						pronunciation = append(pronunciation, elm.Text())
+					}
+				} // else , no IPA
+				//fmt.Fprintf(os.Stderr, "prx children=%02d, title=%s%c", len(elm.Child), title, 10)
+				hg.PRX = strings.Join(pronunciation, ",")
+			case "pr":
+				hg.PR = dumpElm(elm)
+			case "vg": // can be more than one
+				hg.VG = dumpElm(elm)
+			case "lg":
+				hg.LG = dumpElm(elm)
+			case "fg":
+				hg.FG = dumpElm(elm)
+			default:
+				panic(fmt.Sprintf("Unexpected clas name in HG:word=%s, clas=%s", title, className))
+			}
+
+		} else {
+			panic("Unexpected")
+		}
+	}
+	return hg
+}
+
+func dumpTokens(tokens []etree.Token) string {
+	var ss []string
+	for _, tok := range tokens {
+		var s string
+
+		switch tk := tok.(type) {
+		case *etree.Element:
+			//cls := tk.SelectAttr("class")
+			//s = cls.Value
+			s = dumpElm(tk)
+		case *etree.CharData:
+			s = tk.Data
+		default:
+			typ := fmt.Sprintf("<TOK:%T>", tok)
+			panic("Unexpected token type:" + typ)
+		}
+		ss = append(ss, s)
+	}
+	return fmt.Sprintf("[%d:%s]", len(tokens), strings.Join(ss, ","))
+}
+
+func dumpElm(e *etree.Element) string {
+	if e == nil {
+		return "<nil>"
+	}
+	var attrs []string
+	for _, attr := range e.Attr {
+		attrs = append(attrs, attr.Key+"="+attr.Value)
+	}
+	return fmt.Sprintf("<%s %s>%s %s</%s>",
+		e.Tag, strings.Join(attrs, " "),
+		e.Text(),
+		dumpTokens(e.Child),
+		e.Tag)
 }
 
 func S(elm *etree.Element) string {
--- go.mod
+++ go.mod
@@ -9,4 +9,5 @@ require (
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
 	golang.org/x/text v0.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
