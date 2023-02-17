93720bdf7f252aa3b13150896d9e0e5df3bfd9ea
DQNEO <dqneoo@gmail.com>
2023-02-17 15:23:23 +0900

    Automatically find the NOAD file

--- find_dic.sh
+++ find_dic.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find /System/Library/AssetsV2/com_apple_MobileAsset_DictionaryServices_dictionaryOSX -name "Body.data"
+find /System/Library/AssetsV2/com_apple_MobileAsset_DictionaryServices_dictionaryOSX -name "Body.data" | grep -i "New Oxford American Dictionary"
