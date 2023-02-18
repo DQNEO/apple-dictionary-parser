ecc7ddde42a1c188642587ce6e503ded9b545cf7
DQNEO <dqneoo@gmail.com>
2023-02-18 15:32:22 +0900

    Change cli interface

--- extract.go
+++ extract.go
@@ -3,25 +3,38 @@
 
 // Usage:
 //
-//	NOAD_FILE="/System/Library/AssetsV2/com_apple_MobileAsset_DictionaryServices_dictionaryOSX/xxxxxx.asset/AssetData/New Oxford American Dictionary.dictionary/Contents/Resources/Body.data"
-//	./extract $NOAD_FILE > noad.txt
+//	DICT_FILE="/System/Library/AssetsV2/com_apple_MobileAsset_DictionaryServices_dictionaryOSX/xxxxxx.asset/AssetData/New Oxford American Dictionary.dictionary/Contents/Resources/Body.data"
+//	./extract -o noad.txt $DICT_FILE
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/DQNEO/apple-dictionary/cache"
 	"github.com/DQNEO/apple-dictionary/extracter"
+	"io"
 	"os"
 )
 
+var outFilePath = flag.String("o", "", "output file")
+
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "Please specify a dictionary body file\n")
+	flag.Parse()
+	dicFilePath := flag.Arg(0)
+	if dicFilePath == "" {
+		fmt.Fprintf(os.Stderr, "Please specify a dictionary body file.\n For example: '/System/Library/AssetsV2/com_apple_MobileAsset_DictionaryServices_dictionaryOSX/xxxxxx.asset/AssetData/New Oxford American Dictionary.dictionary/Contents/Resources/Body.data'\n")
 		os.Exit(1)
 	}
-	var dicFilePath = os.Args[1]
-	var cacheFile = os.Stdout
-
+	var cacheFile io.Writer
+	if *outFilePath == "" {
+		cacheFile = os.Stdout
+	} else {
+		oFile, err := os.Create(*outFilePath)
+		if err != nil {
+			panic(err)
+		}
+		cacheFile = oFile
+	}
 	entries := extracter.ParseBinaryFile(dicFilePath)
 	cache.SaveEntries(cacheFile, entries)
 }
