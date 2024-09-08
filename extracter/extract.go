1850c39b59decf9b02ba16c5817d025083728fd5
DQNEO <dqneoo@gmail.com>
2024-09-08 19:26:14 +0900

    Refactor

--- extracter/extract.go
+++ extracter/extract.go
@@ -19,57 +19,61 @@ func check(err error) {
 
 // Logic is borrowed from here: https://gist.github.com/josephg/5e134adf70760ee7e49d?permalink_comment_id=4554558#gistcomment-4554558
 func parseBinaryFile(filePath string) [][]byte {
-	var chunks [][]byte
+	var entries [][]byte
 	r, err := os.Open(filePath)
 	check(err)
+	defer r.Close()
+
 	_, err = r.Seek(0x40, io.SeekStart)
 	check(err)
 
-	// read the entire binary size
+	// read the limit marker
 	var limitMarker = make([]byte, 4, 4)
 	_, err = r.Read(limitMarker)
 	check(err)
-	var limitP = (*int32)(unsafe.Pointer(&limitMarker[0]))
+	var limit = *(*int32)(unsafe.Pointer(&limitMarker[0]))
+	entireSize := int64(limit) + 0x40
 
 	_, err = r.Seek(0x60, io.SeekStart)
 	check(err)
+
 	var entryId int
 	var blockIdx int
 	for {
-		pos, err := r.Seek(0, io.SeekCurrent)
+		filePos, err := r.Seek(0, io.SeekCurrent)
 		check(err)
-		if pos >= int64(*limitP)+0x40 {
-			return chunks
+		if filePos >= entireSize {
+			// End of File
+			return entries
 		}
 		blockIdx++
-		var blockSizeBin = make([]byte, 4, 4)
-		_, err = r.Read(blockSizeBin)
+		var blockSizeMarker = make([]byte, 4)
+		_, err = r.Read(blockSizeMarker)
 		if err == io.EOF {
 			fmt.Printf("reached EOF\n")
-			return chunks
+			return entries
 		}
-		var sz *int32 = (*int32)(unsafe.Pointer(&blockSizeBin[0]))
-		if *sz == 0 {
+		var blockSize int32 = *(*int32)(unsafe.Pointer(&blockSizeMarker[0]))
+		if blockSize == 0 {
 			panic("block size is zero")
 		}
 
-		var body = make([]byte, *sz)
-		_, err = r.Read(body)
+		var block = make([]byte, blockSize)
+		_, err = r.Read(block)
 		check(err)
 
-		btsr := bytes.NewReader(body[8:])
-		r, err := zlib.NewReader(btsr)
+		r, err := zlib.NewReader(bytes.NewReader(block[8:]))
 		check(err)
-		buf, err := io.ReadAll(r)
+		blockContents, err := io.ReadAll(r)
 		check(err)
 		chunkPos := 0
-		for chunkPos < len(buf) {
+		for chunkPos < len(blockContents) {
 			entryId++
-			var chunkSizeP *int32 = (*int32)(unsafe.Pointer(&buf[chunkPos]))
+			chunkSize := *(*int32)(unsafe.Pointer(&blockContents[chunkPos]))
 			chunkPos += 4
-			entry := buf[chunkPos : chunkPos+int(*chunkSizeP)]
-			chunks = append(chunks, entry)
-			chunkPos += int(*chunkSizeP)
+			entry := blockContents[chunkPos : chunkPos+int(chunkSize)]
+			entries = append(entries, entry)
+			chunkPos += int(chunkSize)
 		}
 		r.Close()
 	}
